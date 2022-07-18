37e7c301c678f4e9d511cb4180888cf8d34da239
unknown <brandonkimwork@gmail.com>
2022-07-18 09:01:54 +0900

    Programmers Level 1

---
+++ Programmers Level 1/서울에서 김서방 찾기.py
@@ -0,0 +1,6 @@
+def solution(seoul):
+    for i in range(len(seoul)) :
+        if seoul[i] == "Kim":
+            return f"김서방은 {i}에 있다"
+
+#출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
